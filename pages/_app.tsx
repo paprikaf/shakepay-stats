aefc9de9de76a1d90576a33aa665787a447a5a87
Thomas Lefebvre <thomas@unsplash.com>
2021-07-03 16:45:31 -0400

    Install tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import type { AppProps } from 'next/app'
+import "tailwindcss/tailwind.css";
+
+import type { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-export default App
+export default App;
