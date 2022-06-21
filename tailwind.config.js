d3f3fb3dac27f7808765d61366252557a05c2492
Ahmed felfel <felfelahmeed@gmail.com>
2022-06-20 21:01:49 -0400

    purge update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ["./pages/**/*.tsx"],
+  purge: ["./pages/**/*.tsx", "./components/*.tsx"], 
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
