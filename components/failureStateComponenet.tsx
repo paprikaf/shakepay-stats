9a9cb586e6df5802e7a9f3c1c204d2f5f344d1b2
Ahmed felfel <felfelahmeed@gmail.com>
2022-06-29 19:38:47 -0400

    jsx eslint prod fix

--- components/failureStateComponenet.tsx
+++ components/failureStateComponenet.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import * as RemoteData from '@devexperts/remote-data-ts';
 import * as Errors from 'lib/Errors';
-import * as Csv from 'lib/Csv';
 import NavBarComponent from './navBarComponent';
 
 type BtcPriceRemoteData = RemoteData.RemoteData<
@@ -40,9 +39,11 @@ const FailureComponent: React.FC<{
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    style={{
+                      strokeLinecap: 'round',
+                      strokeWidth: '2',
+                      strokeLinejoin: 'round',
+                    }}
                     d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
                   ></path>
                 </svg>
