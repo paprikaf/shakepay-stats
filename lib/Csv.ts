99e20f5cb3f8e5ccb2e4dd503e1ef8934cda83d6
Ahmed felfel <felfelahmeed@gmail.com>
2022-07-21 16:09:11 -0400

    card casheback type error

--- lib/Csv.ts
+++ lib/Csv.ts
@@ -1,9 +1,13 @@
-import { pipe } from "fp-ts/lib/function";
-import * as t from "lib/io-ts";
-import * as E from "fp-ts/lib/Either";
+import { pipe } from 'fp-ts/lib/function';
+import * as t from 'lib/io-ts';
+import * as E from 'fp-ts/lib/Either';
 
-const NumberFromStringOrEmptyString = new t.Type<number, string, unknown>(
-  "NumberFromEmptyString",
+const NumberFromStringOrEmptyString = new t.Type<
+  number,
+  string,
+  unknown
+>(
+  'NumberFromEmptyString',
   t.number.is,
   (u, c) =>
     pipe(
@@ -24,26 +28,26 @@ type NumberFromStringOrEmptyStringC = t.TypeOf<
   typeof NumberFromStringOrEmptyString
 >;
 
-export const fieldName = "csv_file";
+export const fieldName = 'csv_file';
 export enum TransactionType {
-  PurchaseOrSale = "purchase/sale",
-  FiatFunding = "fiat funding",
-  ShakingSats = "shakingsats",
-  CryptoFunding = "crypto funding",
-  PeerTransfer = "peer transfer",
-  FiatCashout = "fiat cashout",
-  CryptoCashout = "crypto cashout",
-  Other = "other",
+  PurchaseOrSale = 'purchase/sale',
+  FiatFunding = 'fiat funding',
+  ShakingSats = 'shakingsats',
+  CryptoFunding = 'crypto funding',
+  PeerTransfer = 'peer transfer',
+  FiatCashout = 'fiat cashout',
+  CryptoCashout = 'crypto cashout',
+  Other = 'other',
 }
 
 export enum DirectionType {
-  Purchase = "purchase",
-  Sale = "sale",
+  Purchase = 'purchase',
+  Sale = 'sale',
 }
 
 export enum CreditDebitType {
-  Debit = "Amount Debited",
-  Credit = "Amount Credited",
+  Debit = 'Amount Debited',
+  Credit = 'Amount Credited',
 }
 
 // {
@@ -84,14 +88,22 @@ export enum CreditDebitType {
 const PurchaseOrSale = t.type(
   {
     Date: t.string,
-    "Amount Debited": t.NumberFromString,
-    "Amount Credited": t.NumberFromString,
-    "Debit Currency": t.union([t.literal("CAD"), t.literal("BTC"), t.literal("ETH")]),
-    "Credit Currency": t.union([t.literal("CAD"), t.literal("BTC") , t.literal("ETH")]),
-    "Buy / Sell Rate": t.union([t.string, t.NumberFromString]), //sometiems there is no buy/sell rate on the CSV.
-    Direction: t.union([t.literal("purchase"), t.literal("sale")]),
+    'Amount Debited': t.NumberFromString,
+    'Amount Credited': t.NumberFromString,
+    'Debit Currency': t.union([
+      t.literal('CAD'),
+      t.literal('BTC'),
+      t.literal('ETH'),
+    ]),
+    'Credit Currency': t.union([
+      t.literal('CAD'),
+      t.literal('BTC'),
+      t.literal('ETH'),
+    ]),
+    'Buy / Sell Rate': t.union([t.string, t.NumberFromString]), //sometiems there is no buy/sell rate on the CSV.
+    Direction: t.union([t.literal('purchase'), t.literal('sale')]),
   },
-  "PurchaseOrSale"
+  'PurchaseOrSale'
 );
 
 // {
@@ -111,15 +123,15 @@ const PurchaseOrSale = t.type(
 const FiatFunding = t.type(
   {
     Date: t.string,
-    "Credit Currency": t.literal("CAD"),
+    'Credit Currency': t.literal('CAD'),
     // "Amount Credited": t.union([t.string, t.NumberFromString]),
-    "Amount Credited": t.NumberFromString,
-    Direction: t.literal("credit"),
+    'Amount Credited': t.NumberFromString,
+    Direction: t.literal('credit'),
 
     // Email usually
-    "Source / Destination": t.string,
+    'Source / Destination': t.string,
   },
-  "FiatFunding"
+  'FiatFunding'
 );
 
 // {
@@ -139,13 +151,13 @@ const FiatFunding = t.type(
 const ShakingSats = t.type(
   {
     Date: t.string,
-    "Credit Currency": t.literal("BTC"),
+    'Credit Currency': t.literal('BTC'),
     // "Amount Credited": t.union([t.string, t.NumberFromString]),
-    "Amount Credited": t.NumberFromString,
-    Direction: t.literal("credit"),
-    "Spot Rate": t.NumberFromString,
+    'Amount Credited': t.NumberFromString,
+    Direction: t.literal('credit'),
+    'Spot Rate': t.NumberFromString,
   },
-  "ShakingSats"
+  'ShakingSats'
 );
 
 // {
@@ -166,14 +178,14 @@ const CryptoFunding = t.type(
   {
     Date: t.string,
     // "Amount Credited": t.union([t.string, t.NumberFromString]),
-    "Amount Credited": t.NumberFromString,
+    'Amount Credited': t.NumberFromString,
     // TODO: if we have an ETH transaction, at the moment the validation will fail.
-    "Credit Currency": t.literal("BTC"),
-    Direction: t.literal("credit"),
-    "Spot Rate": t.NumberFromString,
-    "Blockchain Transaction ID": t.string,
+    'Credit Currency': t.literal('BTC'),
+    Direction: t.literal('credit'),
+    'Spot Rate': t.NumberFromString,
+    'Blockchain Transaction ID': t.string,
   },
-  "CryptoFunding"
+  'CryptoFunding'
 );
 
 // {
@@ -216,30 +228,30 @@ const CryptoFunding = t.type(
 //     }),
 //   ]),
 // ]);
-const EmptyString = t.literal("");
+const EmptyString = t.literal('');
 const PeerTransfer = t.intersection(
   [
     t.type({
       Date: t.string,
-      "Source / Destination": t.string,
-      "Amount Credited": NumberFromStringOrEmptyString,
-      "Amount Debited": NumberFromStringOrEmptyString,
+      'Source / Destination': t.string,
+      'Amount Credited': NumberFromStringOrEmptyString,
+      'Amount Debited': NumberFromStringOrEmptyString,
     }),
     t.union([
       t.type({
-        Direction: t.literal("debit"),
-        "Credit Currency": EmptyString,
-        "Debit Currency": t.string,
+        Direction: t.literal('debit'),
+        'Credit Currency': EmptyString,
+        'Debit Currency': t.string,
       }),
       t.type({
         Date: t.string,
-        Direction: t.literal("credit"),
-        "Credit Currency": t.string,
-        "Debit Currency": EmptyString,
+        Direction: t.literal('credit'),
+        'Credit Currency': t.string,
+        'Debit Currency': EmptyString,
       }),
     ]),
   ],
-  "PeerTransfer"
+  'PeerTransfer'
 );
 
 // {
@@ -260,12 +272,12 @@ const FiatCashout = t.type(
   {
     Date: t.string,
     // "Amount Debited": t.union([t.string, t.NumberFromString]),
-    "Amount Debited": t.NumberFromString,
-    "Debit Currency": t.literal("CAD"),
-    Direction: t.union([t.literal("credit"), t.literal("debit")]),
-    "Source / Destination": t.string,
+    'Amount Debited': t.NumberFromString,
+    'Debit Currency': t.literal('CAD'),
+    Direction: t.union([t.literal('credit'), t.literal('debit')]),
+    'Source / Destination': t.string,
   },
-  "FiatCashout"
+  'FiatCashout'
 );
 
 // {
@@ -285,13 +297,13 @@ const CryptoCashout = t.type(
   {
     Date: t.string,
     // "Amount Debited": t.union([t.string, t.NumberFromString]),
-    "Amount Debited": t.NumberFromString,
-    "Debit Currency": t.literal("BTC"),
-    Direction: t.union([t.literal("credit"), t.literal("debit")]),
-    "Source / Destination": t.string,
-    "Blockchain Transaction ID": t.string,
+    'Amount Debited': t.NumberFromString,
+    'Debit Currency': t.literal('BTC'),
+    Direction: t.union([t.literal('credit'), t.literal('debit')]),
+    'Source / Destination': t.string,
+    'Blockchain Transaction ID': t.string,
   },
-  "CryptoCashout"
+  'CryptoCashout'
 );
 // {
 //   "Transaction Type": "other",
@@ -311,60 +323,62 @@ const CryptoCashout = t.type(
 const Other = t.type(
   {
     Date: t.string,
-    "Amount Credited": NumberFromStringOrEmptyString,
-    "Credit Currency": t.literal("CAD"),
-    Direction: t.literal("credit"),
+    'Amount Credited': NumberFromStringOrEmptyString,
+    'Credit Currency': t.literal('CAD'),
+    Direction: t.literal('credit'),
   },
-  "Other"
+  'Other'
 );
 
-  // {
-  //   "Transaction Type": "card transactions",
-  //   "Date": "2021-12-08T17:25:04+00",
-  //   "Amount Debited": "00.00",
-  //   "Debit Currency": "CAD",
-  //   "Amount Credited": "",
-  //   "Credit Currency": "",
-  //   "Buy / Sell Rate": "",
-  //   "Direction": "debit",
-  //   "Spot Rate": "",
-  //   "Source / Destination": "PROVIDER",
-  //   "Blockchain Transaction ID": ""
-  // },
-const CardTransactions = t.type({
-  "Transaction Type": t.string,
-  Date: t.string,
-  "Amount Debited": t.string,
-  "Debit Currency": t.literal("CAD"),
-  Direction: t.union([t.literal("debit"), t.literal("credit")]),
-  "Source / Destination": t.literal("PROVIDER"),
-},
-"card transactions"
-)
-  // {
-  //   "Transaction Type": "card cashbacks",
-  //   "Date": "2021-12-10T14:30:50+00",
-  //   "Amount Debited": "",
-  //   "Debit Currency": "",
-  //   "Amount Credited": 0,
-  //   "Credit Currency": "BTC",
-  //   "Buy / Sell Rate": "",
-  //   "Direction": "credit",
-  //   "Spot Rate": 62995.7333,
-  //   "Source / Destination": "@cashbacks",
-  //   "Blockchain Transaction ID": ""
-  // }
-  const CardCashbacks = t.type({
-    "Transaction Type": t.string,
-     Date: t.string,
-     "Amount Credited": t.number,
-     "Credit Currencey": t.literal("BTC"),
-     Direction: t.union([t.literal("debit"), t.literal("credit")]),
-     "Spot Rate": t.NumberFromString,
-     "Source / Destination": t.string,
+// {
+//   "Transaction Type": "card transactions",
+//   "Date": "2021-12-08T17:25:04+00",
+//   "Amount Debited": "00.00",
+//   "Debit Currency": "CAD",
+//   "Amount Credited": "",
+//   "Credit Currency": "",
+//   "Buy / Sell Rate": "",
+//   "Direction": "debit",
+//   "Spot Rate": "",
+//   "Source / Destination": "PROVIDER",
+//   "Blockchain Transaction ID": ""
+// },
+const CardTransactions = t.type(
+  {
+    'Transaction Type': t.string,
+    Date: t.string,
+    'Amount Debited': t.string,
+    'Debit Currency': t.literal('CAD'),
+    Direction: t.union([t.literal('debit'), t.literal('credit')]),
+    'Source / Destination': t.string,
   },
-  "card cashbacks"
-  )
+  'card transactions'
+);
+// {
+//   "Transaction Type": "card cashbacks",
+//   "Date": "2021-12-10T14:30:50+00",
+//   "Amount Debited": "",
+//   "Debit Currency": "",
+//   "Amount Credited": 0,
+//   "Credit Currency": "BTC",
+//   "Buy / Sell Rate": "",
+//   "Direction": "credit",
+//   "Spot Rate": 62995.7333,
+//   "Source / Destination": "@cashbacks",
+//   "Blockchain Transaction ID": ""
+// }
+const CardCashbacks = t.type(
+  {
+    'Transaction Type': t.string,
+    Date: t.string,
+    'Amount Credited': t.number,
+    'Credit Currencey': t.literal('BTC'),
+    Direction: t.union([t.literal('debit'), t.literal('credit')]),
+    'Spot Rate': t.NumberFromString,
+    'Source / Destination': t.string,
+  },
+  'card cashbacks'
+);
 
 // TODO: Fix io-ts/lib to allow any property as discriminant
 const createTaggedUnion = <T extends string, C extends t.Mixed>(
@@ -372,36 +386,70 @@ const createTaggedUnion = <T extends string, C extends t.Mixed>(
   codec: C
 ) => {
   return t.intersection([
-    t.type({ "Transaction Type": t.literal(tag) }),
+    t.type({ 'Transaction Type': t.literal(tag) }),
     codec,
   ]);
 };
 
 export const PurchaseOrSaleMember = createTaggedUnion(
-  "purchase/sale",
+  'purchase/sale',
   PurchaseOrSale
 );
-export type PurchaseOrSaleMember = t.TypeOf<typeof PurchaseOrSaleMember>;
-export const FiatFundingMember = createTaggedUnion("fiat funding", FiatFunding);
+export type PurchaseOrSaleMember = t.TypeOf<
+  typeof PurchaseOrSaleMember
+>;
+export const FiatFundingMember = createTaggedUnion(
+  'fiat funding',
+  FiatFunding
+);
 export type FiatFundingMember = t.TypeOf<typeof FiatFundingMember>;
-export const ShakingStatsMember = createTaggedUnion("shakingsats", ShakingSats);
+export const ShakingStatsMember = createTaggedUnion(
+  'shakingsats',
+  ShakingSats
+);
 export type ShakingStatsMember = t.TypeOf<typeof ShakingStatsMember>;
-export const CryptoFundingMember = createTaggedUnion("crypto funding",CryptoFunding);
-export type CryptoFundingMember = t.TypeOf<typeof CryptoFundingMember>;
-export const PeerTransferMember = createTaggedUnion( "peer transfer",PeerTransfer);
+export const CryptoFundingMember = createTaggedUnion(
+  'crypto funding',
+  CryptoFunding
+);
+export type CryptoFundingMember = t.TypeOf<
+  typeof CryptoFundingMember
+>;
+export const PeerTransferMember = createTaggedUnion(
+  'peer transfer',
+  PeerTransfer
+);
 export type PeerTransferMember = t.TypeOf<typeof PeerTransferMember>;
-export const FiatCashoutMember = createTaggedUnion("fiat cashout", FiatCashout);
+export const FiatCashoutMember = createTaggedUnion(
+  'fiat cashout',
+  FiatCashout
+);
 export type FiatCashoutMember = t.TypeOf<typeof FiatCashoutMember>;
-export const CryptoCashoutMember = createTaggedUnion("crypto cashout",CryptoCashout);
-export type CryptoCashoutMember = t.TypeOf<typeof CryptoCashoutMember>;
-export const OtherMember = createTaggedUnion("other", Other);
+export const CryptoCashoutMember = createTaggedUnion(
+  'crypto cashout',
+  CryptoCashout
+);
+export type CryptoCashoutMember = t.TypeOf<
+  typeof CryptoCashoutMember
+>;
+export const OtherMember = createTaggedUnion('other', Other);
 export type OtherMember = t.TypeOf<typeof OtherMember>;
-export const CardCashbacksMember = createTaggedUnion("card cashbacks", CardCashbacks);
-export type CardCashbacksMember = t.TypeOf<typeof CardCashbacksMember>
-export const CardTransactionsMember = createTaggedUnion("card transactions", CardTransactions);
-export type CardTransactionsMember = t.TypeOf<typeof CardTransactionsMember>
+export const CardCashbacksMember = createTaggedUnion(
+  'card cashbacks',
+  CardCashbacks
+);
+export type CardCashbacksMember = t.TypeOf<
+  typeof CardCashbacksMember
+>;
+export const CardTransactionsMember = createTaggedUnion(
+  'card transactions',
+  CardTransactions
+);
+export type CardTransactionsMember = t.TypeOf<
+  typeof CardTransactionsMember
+>;
 
-export const CsvT = t.taggedUnion("Transaction Type", [
+export const CsvT = t.taggedUnion('Transaction Type', [
   PurchaseOrSaleMember,
   FiatFundingMember,
   ShakingStatsMember,
@@ -429,15 +477,14 @@ export type Csv = t.TypeOf<typeof CsvT>;
 
 export const Csv = t.createStructuredTaggedUnion(CsvT);
 
-
 export const ResponseT = t.type({
-  "crypto cashout": t.number,
-  "crypto funding": t.number,
-  "fiat cashout": t.number,
-  "fiat funding": t.number,
+  'crypto cashout': t.number,
+  'crypto funding': t.number,
+  'fiat cashout': t.number,
+  'fiat funding': t.number,
   other: t.number,
-  "peer transfer": t.number,
-  "purchase/sale": t.number,
+  'peer transfer': t.number,
+  'purchase/sale': t.number,
   shakingsats: t.number,
 });
 
